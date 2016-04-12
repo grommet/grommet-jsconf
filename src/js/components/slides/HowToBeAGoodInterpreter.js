fd4b858a2fcf0a3658a66668e8eef11505ced6cb
Alan Souza <alansouzati@gmail.com>
2016-04-12 00:28:34 -0700

    Fixed few issues with presentation

--- src/js/components/slides/HowToBeAGoodInterpreter.js
+++ src/js/components/slides/HowToBeAGoodInterpreter.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Anchor from 'grommet/components/Anchor';
+
 import Bullet from 'grommet-presentation/components/Bullet';
 import Bullets from 'grommet-presentation/components/Bullets';
 
@@ -12,6 +14,13 @@ const HowToBeAGoodInterpreter = () => {
           <Bullet item='Paragraph is not an Anchor' />
           <Bullet item='Custom components must have roles (e.g. menu)' />
           <Bullet item='much more...' />
+        </Bullets>
+      </Bullet>
+      <Bullet item='Understand and follow WCAG guidelines'>
+        <Bullets>
+          <Anchor href='https://www.w3.org/TR/WCAG20/' target='_blank'>
+            Web Content Accessibility Guidelines
+          </Anchor>
         </Bullets>
       </Bullet>
       <Bullet item='Choose colors that have good contrast ratio' />
